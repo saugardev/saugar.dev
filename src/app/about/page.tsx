d6bc49d4b6ad345ff9d875faba2f5bc07c0a8324
Saúl García Céspedes <saul@dixwe.com>
2023-08-08 21:45:58 +0200

    feat: update app description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div className="space-y-2 px-5">
       <p className="text-xl font-semibold leading-7 text-primary">Hello, I am</p>
-      <h1 className="scroll-m-20 text-5xl font-bold tracking-tight">Saúl García Céspedes</h1>
+      <h1 className="scroll-m-20 text-5xl font-bold tracking-tight">Saul Garcia Cespedes</h1>
       <p className="text-xl text-muted-foreground">Full Stack Dev.</p>
     </div>
   )
