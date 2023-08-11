d554516ff6e3cf28061ee75b4ce5a5c18fdcaee8
Saúl García Céspedes <saul@dixwe.com>
2023-08-11 15:04:03 +0200

    feat: about me page with mdx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,45 @@
-export default function About() {
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import Button from '@/components/mdx/Button';
+
+import remarkGfm from 'remark-gfm'
+
+const options = {
+  mdxOptions: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  }
+}
+
+function Article() {
+
+  function readFile(filePath: string) {
+    if (fs.statSync(filePath).isFile()) {
+      const fileContent = fs.readFileSync(filePath, 'utf-8');
+      const { data: frontMatter, content } = matter(fileContent);
+      return {
+        meta: frontMatter,
+        slug: path.parse(filePath).name,
+        content
+      };
+    }
+    throw new Error('Given path does not point to a file');
+  }
+  
+  const articlePath = "src/mdx/about/me.mdx";
+  const article = readFile(articlePath);
+
+  const { meta, content } = article;
+  
   return (
-    <div className="space-y-2">
-      {/* <p className="text-xl font-semibold leading-7 text-primary mx-5">Hello, I am</p> */}
-      <h1 className="scroll-m-20 text-5xl font-bold tracking-tight mx-5 mb-7">Saul Garcia Cespedes</h1>
-      <p className="text-lg text-muted-foreground mx-5 py-3">Full Stack Dev.</p>
-    </div>
+    <article className='prose prose-md md:prose-base lg:prose-lg dark:prose-invert prose-stone lg:mx-0 mx-auto lg:px-5 p-0'>
+      <h1 className="scroll-m-20 text-5xl font-bold tracking-tight">{meta.title}</h1>
+      <MDXRemote source={content} components={{Button}} options={options}/>
+    </article>
   )
 }
+
+export default Article;
