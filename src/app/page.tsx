431f8743002e02fe593d7405c25d82c659564158
Saúl García Céspedes <saul@dixwe.com>
2023-08-12 18:13:21 +0200

    feat: add old hashnode blog and sort by date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,15 @@ export default function Article() {
   }).map(filename => {
     const fileContent = fs.readFileSync(path.join(articlesDir, filename), 'utf-8');
     const { data: frontMatter } = matter(fileContent);
+    const [day, month, year] = frontMatter.date.split('-').map(Number);
+    const date = new Date(year, month - 1, day);
     return {
       meta: frontMatter,
+      date,
       slug: path.parse(filename).name
     };
-  });
-
+  }).sort((a, b) => b.date.getTime() - a.date.getTime());
+  
   const datesWithUrls = articles.map((article) => {
     const dateStr = article.meta.date;
     const [day, month, year] = dateStr.split('-').map(Number);
@@ -34,7 +37,7 @@ export default function Article() {
       <div className='flex-grow'>
         <h1 className="scroll-m-20 text-5xl font-bold tracking-tight mx-5">Articles</h1>
         <section className='py-5 max-w-2xl'>
-          <div>
+          <div className='flex flex-col gap-3'>
             {articles.map(article => (
               <Link className='py-3 px-5 flex justify-between align-middle gap-2 hover:bg-accent dark:hover:text-primary rounded-md focus:bg-accent focus-visible:text-primary focus:outline-none focus-visible:outline-none' href={'/' + article.slug} passHref key={article.slug}>
                 <div className='w-full'>
