fed5b4309b5b14200cc940a1e52e30ec0778b0c1
Saúl García Céspedes <saul@dixwe.com>
2023-08-14 16:50:12 +0200

    chore: remove unused dependencies

--- next.config.js
+++ next.config.js
@@ -1,11 +1,2 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-})
- 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  reactStrictMode: true,
-}
- 
 module.exports = {}
